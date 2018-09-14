d42f372dc2c8ad34060b79c8584bad4e75d6f3eb
Joy <myjoybar@gmail.com>
2018-09-14 16:55:51 +0800

    Update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,15 @@
     <div class="main-home">
         <h1>{{ msg }}</h1>
         <div>
-            <button class="btn" v-on:click="greetRemote2">网络请求</button>
+            <input v-model='commodity' placeholder="请输入商品名" type="text"/>
+            <button class="btn" v-on:click="greetRemote2">搜索</button>
+        </div>
+        <div>
+            <ul id="v-for-object" class="demo">
+                <li v-for="value in pageList">
+                    {{ value.title }}
+                </li>
+            </ul>
         </div>
     </div>
 </template>
@@ -10,98 +18,79 @@
 <script>
     const API_PROXY = 'https://bird.ioliu.cn/v1/?url='
     import axios from 'axios'
+    import {isEmpty} from '../../static/js/StringUtils.js'
+    import {print} from '../../static/js/Log.js'
+
     export default {
         name: 'home',
         data() {
             return {
-                msg: ' I am page home',
+                msg: ' 搜索淘宝客商品',
                 dataURL: "http://pub.alimama.com",
                 method: "/items/search.json",
-                //  method: "/items/search.json?q=男装",
-
-               // dataURL: "https://www.baidu.com/",
-               // method: "",
+                pageList: null,
             }
-        }, methods: {
+        },
+        methods: {
 
             greetRemote2: function (event) {
+                var self = this;
+                if (isEmpty(this.commodity)) {
+                    log("商品名不能为空");
+                    return
+                }
+
                 axios({
                     method: "get",
-                    url: API_PROXY+ this.dataURL + this.method,
+                    commodity: "",
+                    url: API_PROXY + this.dataURL + this.method,
                     headers: {
                         'Content-Type': 'application/x-www-form-urlencoded',
                     },
                     params: {
-                        q: '连衣裙'
+                        q: this.commodity
                     }
                 }).then((res) => {
-                        console.log(res);
+                    print(res);
+                    print(res.data);
+                     //   print(res.data.url);
+                        // self.pageList = res.data.pageList
+                        // print(self.pageList);
                     }
                 ).catch((err) => {
-                        console.log(err);
+                    print(err);
                     }
                 );
 
             },
-            greetRemote: function (event) {
-                let params= {
-                    q: '连衣裙'
-                };
 
-                axios.get(this.dataURL+this.method, qs.stringify(params))
-                    .then(response => {
-                        console.log(response);
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
+            greetRemote3: function (event) {
 
-            },
-
-
-            fetchData: function () {
-
-                // this.$jsonp(this.dataURL+this.method,
-                //     Object.assign(this.$store.state.common, {q: '连衣裙'})
-                // ).then(json => {
-                //     _this.pageData = json.result
-                //     console.log(json.result);
-                // }).catch(err => {
-                //     console.log(err)
-                // })
-
-                this.$jsonp(this.dataURL+this.method,
-                    {
-                        q: '连衣裙',
-                        // jsonp:'cb'
-
+                axios({
+                    method: "get",
+                    commodity: "",
+                    url: API_PROXY + this.dataURL + this.method,
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Content-Type': 'application/x-www-form-urlencoded',
                     },
-                ).then(json => {
-                    // 返回数据 json， 返回的数据就是json格式
-                }).catch(err => {
-                    console.log(err)
-                })
-
-            },
-
-            fetchData2: function () {
-                let params= {
-                    q: '连衣裙'
-                };
-
-                getRecommend(this.dataURL+this.method,"","").then((res) => {
-                    if (res.code === ERR_OK) {
-                        console.log("ERR_OK")
-                    }else{
-                        console.log("err")
+                    params: {
+                        q: this.commodity
                     }
-                })
+                }).then((res) => {
+                    print(res);
+                    print(res.data);
+                        //   print(res.data.url);
+                        // self.pageList = res.data.pageList
+                        // print(self.pageList);
+                    }
+                ).catch((err) => {
+                    print(err);
+                    }
+                );
+
             },
-
-
-
-
-
 
         }
     }
@@ -110,7 +99,7 @@
     .main-home {
         text-align center
         h1 {
-            font-size 20px
+            font-size 28px
             color rgba(175, 47, 47, 0.4)
             margin 20px
         }
---
+++ static/js/Log.js
@@ -0,0 +1,36 @@
+
+const debug = true
+function print(msg){
+   if(debug){
+       if(isEmpty(msg)){
+           console.log("is empty");
+           return
+       }
+       if(isJson(msg)){
+          // console.log(JSON.stringify(msg));
+           console.log(msg);
+       }else{
+           console.log(msg);
+       }
+   }
+}
+
+
+function isEmpty(obj){
+    if(typeof obj == "undefined" || obj == null || obj == ""){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function isJson(obj) {
+    var isjson = typeof(obj) == "object" && Object.prototype.toString.call(obj).toLowerCase() == "[object object]" && !obj.length;
+    return isjson;
+}
+
+export {
+    print,
+}
+
+
